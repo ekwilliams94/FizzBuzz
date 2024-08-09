d8e37adea3d9fda92c23175e206f16d77ae01805
ekwilliams94 <35872428+ekwilliams94@users.noreply.github.com>
2024-08-09 14:05:50 +0100

    Implement method in main

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace FizzBuzz
+﻿using System.Reflection;
+
+namespace FizzBuzz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var serviceType = Assembly.GetExecutingAssembly().GetType("FizzBuzz.FizzBuzzService");
+
+            if (serviceType != null)
+            {
+                var serviceInstance = Activator.CreateInstance(serviceType);
+                var methodInfo = serviceType.GetMethod("GetFizzBuzzOutput");
+
+                if (methodInfo != null && serviceInstance != null)
+                {
+                    for(int i = 1; i <= 100; i++)
+                    {
+                        var result = methodInfo.Invoke(serviceInstance, [i]);
+                        Console.WriteLine(result);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Method not found");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Type not found");
+            }
             Console.ReadLine();
         }
     }
